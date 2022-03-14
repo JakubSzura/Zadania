9d9deb5c45fe89b544f88b7f5609aeeca895d2a2
JakubSzura <JakubSzara06@gmail.com>
2022-03-14 11:02:49 +0100

    kom

---
+++ NWW/NWW.cpp
@@ -0,0 +1,31 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, ilo,nww;
+
+
+    cin >> a;
+    cin >> b;
+
+    ilo = a * b;
+    while (a != b) {
+
+        if (a < b) {
+            b = b - a;
+        }
+        else {
+            a = a - b;
+        }
+      
+
+    }
+    nww = ilo / a;
+
+    cout << nww;
+
+
+}
+
