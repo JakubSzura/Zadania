ca9cc147f5f5cc0a8e1b407423a753ea151065c2
JakubSzura <JakubSzara06@gmail.com>
2022-03-14 10:54:46 +0100

    NWD

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,25 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout << "Podaj pierwszą liczbę: ";
+    cin >> a;
+    cout << "Podaj drugą liczbę: ";
+    cin >> b;
+    while (a != b) {
+        if (a < b) {
+            b = b - a;
+        }
+        else {
+            a = a - b;
+        }
+        
+    }
+
+    cout <<"Największy wspólny dzielnik: " << a;
+
+}
+
--- LiczbaPierwsza/LiczbaPierwsza.cpp
+++ LiczbaPierwsza/LiczbaPierwsza.cpp
@@ -1,17 +1,46 @@
 ﻿
 #include <iostream>
 using namespace std;
+
 int main()
 {
-    int lp = 0;
-    int p = 2;
-    int n;
-    cout << "do której liczby";
-    cin >> n;
+	int lp = 0;
+	int p = 2;
+	int d;
+	int n;
+
+	cout << "do której liczby:" << endl;
+	cin >> n;
+	bool t;
+	while (lp < n) {
+		t = true;
+		for (d = 2; d < p ; d++) {
+			
+			if (p % d == 0) 
+			{
+				t = false;
+				break;
+			}
+		if ( t )
+		{
+			cout << p << " ";
+			lp++;
+		}
+		p++;
+	}
+	cout << endl;
+	return 0;
+}
 
 
 
 
 
 }
+
+
+
+
+
+
 
