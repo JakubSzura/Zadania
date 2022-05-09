48c4edc830057d96612a6b48f883f465c6188b40
JakubSzura <JakubSzara06@gmail.com>
2022-05-09 10:37:42 +0200

    Szyfr

---
+++ Szyfr Cezara/Szyfr Cezara.cpp
@@ -0,0 +1,54 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+
+
+
+
+int main()
+{
+    char tab[1000];
+    int klucz;
+    int dl;
+
+
+    cout << "Podaj wyraz skladajacy sie z malych liter: ";
+    cin >> tab;
+
+    cout << "Podaj klucz z przedzialu [-26..26]: ";
+    cin >> klucz;
+
+    dl = strlen(tab);
+
+    if (!(klucz >= -26 && klucz <= 26)) {
+        return(0);
+    }
+
+    if (klucz >= 0) {
+        for (int i = 0; i < dl; i++) {
+            if (tab[i] + klucz <= 'z') {
+                tab[i] += klucz;
+
+            }
+            else {
+                tab[i] = tab[i] + klucz - 26;
+            }
+        }
+    }
+    else {
+        for (int i = 0; i < dl; i++) {
+            if (tab[i] + klucz >= 'a') {
+                tab[i] += klucz;
+            }
+            else {
+                tab[i] = tab[i] + klucz + 26;
+            }
+        }
+    }
+
+    cout << tab;
+
+
+}
